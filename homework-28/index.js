111273319156b6fa7611e76c18026a2c24c806d4
bondrnk <hanna.bondarenko.s@gmail.com>
2021-10-19 00:26:06 +0300

    Added homework-28

--- homework-28/index.js
+++ homework-28/index.js
@@ -3,7 +3,7 @@ const errorSwal = (text) => swal({ text, icon: 'error' });
 const warningSwal = (text) => swal({ text, icon: 'warning' });
 
 // Button clicks
-for (let i = 1; i <= 5; i++) {
+for (let i = 1; i <= 2; i++) {
   const button = document.getElementById('js-button' + i);
 
   button.addEventListener('click', this['task' + i]);
@@ -11,186 +11,234 @@ for (let i = 1; i <= 5; i++) {
 
 // Task 1
 async function task1() {
-  const numberFrom = +await swal({
-    text: 'Введіть ціле число від якого рахувати:',
+  const inputCarBrand = await swal({
+    text: 'Введіть бренд авто:',
     content: {
       element: 'input',
       attributes: {
-        placeholder: '1',
-        type: "number"
+        placeholder: 'BMW',
+        type: 'text'
       }
     }
   });
-  const numberTo = +await swal({
-    text: 'І число до якого рахувати (включно):',
-    content: {
-      element: 'input',
-      attributes: {
-        placeholder: '99',
-        type: "number"
-      }
-    }
-  });
-  let sum = null;
 
-  // Перевірка чи ввели числа і чи вони цілі
-  if (
-    Number.isInteger(numberFrom)
-    && Number.isInteger(numberTo)
-    && numberFrom.toString
-    && numberTo.toString
-  ) {
-    // Перевірка чи коректний діапазон ввели
-    if (numberFrom <= numberTo) {
-      for (let i = numberFrom; i <= numberTo; i++) {
-        sum += i;
+  const inputCarModel = await swal({
+    text: 'Введіть модель авто:',
+    content: {
+      element: 'input',
+      attributes: {
+        placeholder: 'X6',
+        type: 'text'
       }
-      successSwal('Сума всіх чисел в діапазоні (' + numberFrom + ', ' + numberTo + ') = ' + sum + '!');
-    } else {
-      errorSwal('Діапазон введено неправильно!');
     }
-  } else {
-    errorSwal('Введіть цілі числа!');
+  });
+
+  const inputCarYear = await swal({
+    text: 'Введіть рік випуску авто:',
+    content: {
+      element: 'input',
+      attributes: {
+        placeholder: '2021',
+        type: 'number'
+      }
+    }
+  });
+
+  const inputCarAverageSpeed = await swal({
+    text: 'Введіть середню швидкість авто:',
+    content: {
+      element: 'input',
+      attributes: {
+        placeholder: '180',
+        type: 'number'
+      }
+    }
+  });
+
+  const userCar = {
+    brand: inputCarBrand || 'BMW',
+    model: inputCarModel || 'X6',
+    year: inputCarYear || '2021',
+    averageSpeed: inputCarAverageSpeed || '180'
+  };
+
+  const getCarInfo = (car) => `
+    Бренд: ${car.brand}
+    Модель: ${car.model}
+    Рік: ${car.year}
+    Середня швидкість: ${car.averageSpeed} км/год
+  `;
+
+  const value = await swal({
+    buttons: {
+      info: {
+        text: 'Інформація про введену машину',
+        value: 'info'
+      },
+      countTime: {
+        text: 'Розрахувати необхідий час на введену відстань',
+        value: 'countTime'
+      }
+    }
+  });
+
+  switch (value) {
+    case 'info': {
+      successSwal(`Інформація про введену машину: ${getCarInfo(userCar)}`);
+      break;
+    }
+    case 'countTime': {
+      const inputDistance = await swal({
+        text: 'Введіть відстань (км):',
+        content: {
+          element: 'input',
+          attributes: {
+            placeholder: '1000',
+            type: 'number'
+          }
+        }
+      }) || '1000';
+
+      if (inputDistance <= 0) {
+        errorSwal('Введіть значення більше 0!');
+        break;
+      }
+
+      const countTime = (car, distance) => {
+        let time = distance / car.averageSpeed;
+        time += Math.trunc(time / 4);
+        return time;
+      };
+
+      successSwal(`Даній машині необхідно ${countTime(userCar, inputDistance).toFixed(2)} годин(и) для подолання відстані ${inputDistance} км при середній швидкості ${userCar.averageSpeed} км/год`);
+      break;
+    }
   }
 }
 
+
 // Task 2
 async function task2() {
-  const number1 = +await swal({
-    text: 'Введіть число №1:',
+  const numerator1 = +await swal({
+    text: 'Введіть чисельник першого числа',
     content: {
       element: 'input',
       attributes: {
         placeholder: '1',
-        type: "number"
+        type: 'number'
       }
     }
-  });
-  const number2 = +await swal({
-    text: 'Введіть число №2:',
+  }) || 1;
+
+  const denominator1 = +await swal({
+    text: 'Введіть знаменник першого числа',
     content: {
       element: 'input',
       attributes: {
         placeholder: '2',
-        type: "number"
+        type: 'number'
       }
     }
-  });
+  }) || 2;
 
-  function NOD(x, y) {
-    // Коли y = 0, то повертаємо х (тобто це найбільший дільник який залишився)
-    if (!y) return Math.abs(x);
-    return NOD(y, x % y);
-  }
-
-  // Перевірка чи ввели числа і чи вони цілі
-  if (
-    Number.isInteger(number1)
-    && Number.isInteger(number2)
-    && number1.toString
-    && number2.toString
-  ) {
-    if (number1 && number2) {
-      successSwal('Найбільший дільник чисел ' + number1 + ' і ' + number2 + ' = ' + NOD(number1, number2) + '!');
-    } else {
-      // Якщо 0 - то дільник тільки 1
-      successSwal('Найбільший дільник чисел ' + number1 + ' і ' + number2 + ' = 1!');
-    }
-  } else {
-    errorSwal('Введіть цілі числа!');
-  }
-}
-
-// Task 3
-async function task3() {
-  const number = +await swal({
-    text: 'Введіть число:',
+  const numerator2 = +await swal({
+    text: 'Введіть чисельник другого числа',
     content: {
       element: 'input',
       attributes: {
-        placeholder: '1',
-        type: "number"
+        placeholder: '3',
+        type: 'number'
       }
     }
-  });
-  // Задаємо пустий рядок (бо інакше в рядок виводиться null, undefined, NaN і т.д.)
-  let dividers = '';
+  }) || 3;
 
-  // Перевірка чи ввели число і чи воно ціле
-  if (Number.isInteger(number) && number.toString) {
-    // Якщо 0 - то дільник тільки 1
-    if (!number) {
-      dividers = 1;
-    } else {
-      for (var i = 1; i <= Math.abs(number); i++) {
-        // Додаємо саме число (без коми)
-        if (i === Math.abs(number)) {
-          dividers += i.toString();
-          break;
-        }
-        // Якщо це дільник то додаємо число з комою
-        if (!(number % i)) {
-          dividers += (i.toString() + ', ');
-        }
-      }
-    }
-    successSwal('Дільники числа ' + number + ': (' + dividers + ')!');
-  } else {
-    errorSwal('Введіть ціле число!');
-  }
-}
-
-// Task 4
-async function task4() {
-  const number = await swal({
-    text: 'Введіть число:',
+  const denominator2 = +await swal({
+    text: 'Введіть знаменник другого числа',
     content: {
       element: 'input',
       attributes: {
-        placeholder: '1',
-        type: "number"
+        placeholder: '4',
+        type: 'number'
+      }
+    }
+  }) || 4;
+
+  const number1 = {
+    numerator: numerator1,
+    denominator: denominator1
+  };
+
+  const number2 = {
+    numerator: numerator2,
+    denominator: denominator2
+  };
+
+  const NSD = (n1, n2) => {
+    if (!n2) return Math.abs(n1);
+    return NSD(n2, n1 % n2);
+  }
+
+  const NSK = (n1, n2) => {
+    return n1 * n2 / NSD(n1, n2);
+  }
+
+  const nsk = NSK(number1.denominator, number2.denominator);
+
+  const multiplier1 = nsk / number2.denominator;
+  const multiplier2 = nsk / number1.denominator;
+
+  const simplify = (n) => {
+    let divider = 1;
+    for (let i = 2; i < Math.min(Math.abs(n.numerator), Math.abs(n.denominator)); i++) {
+      if (n.numerator % i === 0 && n.denominator % i === 0) {
+        divider = i;
+      }
+    }
+    n.numerator /= divider;
+    n.denominator /= divider;
+    return `${n.numerator}/${n.denominator}`
+  }
+
+  const operations = {
+    sum: (n1, n2) => `${n1.numerator * multiplier1 + n2.numerator * multiplier2}/${nsk}`,
+    subtract: (n1, n2) => `${n1.numerator * multiplier1 - n2.numerator * multiplier2}/${nsk}`,
+    multiply: (n1, n2) => `${n1.numerator * n2.numerator}/${n1.denominator * n2.denominator}`,
+    divide: (n1, n2) => `${n1.numerator * n2.denominator}/${n1.denominator * n2.numerator}`,
+    simplify: (n1, n2) => `${simplify(n1)}, ${simplify(n2)}`
+  };
+
+  const symbol = {
+    sum: ' + ',
+    subtract: ' - ',
+    multiply: ' * ',
+    divide: ' ÷ ',
+    simplify: ', '
+  };
+
+  const value = await swal({
+    buttons: {
+      sum: {
+        text: 'Додати',
+        value: 'sum'
+      },
+      subtract: {
+        text: 'Відняти',
+        value: 'subtract'
+      },
+      multiply: {
+        text: 'Помножити',
+        value: 'multiply'
+      },
+      divide: {
+        text: 'Поділити',
+        value: 'divide'
+      },
+      simplify: {
+        text: 'Спростити',
+        value: 'simplify'
       }
     }
   });
 
-  successSwal('В числі ' + number + ' - ' + number.split('').length + ' цифр(и)!');
-}
-
-// Task 5
-async function task5() {
-  let numberCount = 0;
-  let positiveNumbers = 0;
-  let negativeNumbers = 0;
-  let nullNumbers = 0;
-  let evenNumbers = 0;
-  let oddNumbers = 0;
-  let numberList = '';
-
-  while (numberCount < 10) {
-    numberCount++;
-    const number = +await swal({
-      text: 'Введіть число №' + numberCount + ':',
-      content: {
-        element: 'input',
-        attributes: {
-          placeholder: '0',
-          type: "number"
-        }
-      }
-    });
-
-    numberList += (number.toString() + ((numberCount !== 10) ? ', ' : ''));
-
-    if (number > 0) {
-      positiveNumbers++;
-    } else if (number < 0) {
-      negativeNumbers++;
-    } else {
-      nullNumbers++;
-    }
-
-    (number % 2) ? oddNumbers++ : evenNumbers++;
-  }
-
-  successSwal('Було введено: ' + numberList + '\n\n- додатніх чисел: ' + positiveNumbers + '\n- від\'ємних чисел: ' + negativeNumbers + '\n- нулів або пустих чисел: ' + nullNumbers + '\n\n- парних чисел: ' + evenNumbers + '\n- непарних чисел: ' + oddNumbers);
+  successSwal(number1.numerator + '/' + number1.denominator + symbol[value] + number2.numerator + '/' + number2.denominator + ' = ' + operations[value](number1, number2));
 }
